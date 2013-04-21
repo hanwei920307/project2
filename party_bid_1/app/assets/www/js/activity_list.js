af2445fb9270c3f4c2f1ce936c6746ec12878517
= <=>
2013-04-21 17:26:18 +0800

    finish_btn_status_control

--- party_bid_1/app/assets/www/js/activity_list.js
+++ party_bid_1/app/assets/www/js/activity_list.js
@@ -1,14 +1,15 @@
 function make_disable_and_enable_button()
 {
+    window.location="#create_activity_page"
     $("#activity_list_text").keyup(function(){
     var text_content=$("#activity_list_text").val();
         if(text_content.trim()=="")
         {
-            $("#create_act_button").button("disable")
+            $("#create_button").button("disable")
         }
         else
         {
-            $("#create_act_button").button("enable");
+            $("#create_button").button("enable");
         }
     })
 }
@@ -16,7 +17,6 @@ function make_disable_and_enable_button()
 function get_activity_name()
 {
     var Name=$("#activity_list_text").val()
-    localStorage.start_activity_name= $("#activity_list_text").val()
     var activity ={"name":Name,"count":0}
     localStorage.activity_names=localStorage.activity_names || '[]'
     var activity_name=JSON.parse(localStorage.activity_names)
@@ -33,9 +33,21 @@ function save_activity_name()
 {
     get_activity_name()
     display_activity_name()
+    localStorage.now_activity_page_name = $("#activity_list_text").val();
     clear_activity_list_text()
     window.location.href="#sign_up_page";
-    $("#create_act_button").button("disable")
+    $("#create_button").button("disable")
+}
+
+function create_button()
+{
+    localStorage.now_activity_page_count =0
+    try
+    {
+        $("#start").text("开始")
+        $("#start_activity_button").button("enable")
+    }   catch (e) {}
+
 }
 
 function activity_name_list()
@@ -46,7 +58,6 @@ function activity_name_list()
     {
         str += '<li><a onclick="change_button_name(' + i + ')" >' + activity_name[i]["name"]  + '</a></li>'
     }
-    console.log("a")
         return str;
 }
 
@@ -73,6 +84,7 @@ function display_activity_name()
 
 function save_and_judge_activity_name()
 {
+    console.log(" save_and_judge_activity_name() begin")
     try
     {
         var activity_name=JSON.parse(localStorage.activity_names)
@@ -87,41 +99,71 @@ function save_and_judge_activity_name()
         }
     }catch (e) {}
     save_activity_name();
+    create_button()
 }
+
 
 function change_button_name(activity_name)
 {
     window.location="#sign_up_page"
     var activity=JSON.parse(localStorage.activity_names)
-    localStorage.start_activity_name=activity[activity_name]["name"]
+    localStorage.now_activity_page_count=activity[activity_name]["count"]
+    localStorage.now_activity_page_name=activity[activity_name]["name"]
     var start_count=0;
+    var start2=0;
     for(var i=0;i<activity.length;i++)
     {
+        console.log("for begin")
         if(activity[i]["count"]==1)
         {
-             start_count ++;
+             start_count=1;
+             start2=1;
 
         }
+        if(activity[i]["count"]==2)
+        {
+            start_count=2
+        }
     }
+        if(activity[activity_name]["count"]==0 && start_count==0 )
+        {
+            console.log("if(activity[activity_name][count]==0 && start_count==0 )")
+             $("#start").text("开始")
+             $("#start_activity_button").button("enable")
+        }
+        else if(activity[activity_name]["count"]==0 && start_count==0)
+        {
+             $("#start").text("开始")
+             $("#start_activity_button").button("enable")
+        }
+        else if(activity[activity_name]["count"]==0 && start_count==1)
+        {
+            $("#start").text("开始")
+            $("#start_activity_button").button("disable")
+        }
+        else if(activity[activity_name]["count"]==0 && start_count==2 && start2==1)
+        {
+            $("#start").text("开始")
+            $("#start_activity_button").button("disable")
+        }
+        else if(activity[activity_name]["count"]==1)
+        {
+            $("#start").text("结束")
+            $("#start_activity_button").button("enable")
+        }
+        else if(activity[activity_name]["count"]==0 && start_count==2)
+        {
+            $("#start").text("开始")
+            $("#start_activity_button").button("enable")
+        }
 
-    if(activity[activity_name]["count"]==0 && start_count!=1 )
-    {
-        window.location="#sign_up_page"
-        $("#start").text("开始")
-        $("#start_button").button("enable")
-    }
-    if(activity[activity_name]["count"]==1)
-    {
-        window.location="#sign_up_page"
-        $("#start").text("结束")
-        $("#start_button").button("disable")
-    }
-    if(activity[activity_name]["count"]==2)
-    {
-        window.location="#sign_up_page"
-        $("#start").text("结束")
-        $("#start_button").button("disable")
-    }
+        else if(activity[activity_name]["count"]==2)
+        {
+            $("#start").text("结束")
+            $("#start_activity_button").button("disable")
+        }
+        console.log("for end");
+
 }
 
 function  click_start_to_end_button()
@@ -129,94 +171,72 @@ function  click_start_to_end_button()
     var activity=JSON.parse(localStorage.activity_names)
     for(var i=activity.length-1;i>=0;i--)
     {
-        if(activity[i]["name"]==localStorage.start_activity_name)
+        if(activity[i]["name"]==localStorage.now_activity_page_name)
         {
             activity[i]["count"]=1
+            localStorage.now_activity_page_count=activity[i]["count"]
         }
     }
     localStorage.activity_names=JSON.stringify(activity)
     $("#start").text("结束")
 }
- /*
-//function make_button_disable()
+
+function create_activity_btn_disable()
 {
     var activity=JSON.parse(localStorage.activity_names)
-    for(var i=0;i<activity.length;i++)
-    {
-        if(activity[i]["count"]==0)
-        {
-            $("#start").text("开始")
-            $("#start_button").button("enable")
-        }
-        else(activity[i]["count"]==1)
-        {
-            $("#start_button").button("disable")
-        }
-
-    }
-}
-
-/*function click_start_to_end_button()
-{
-    var activity=JSON.parse(localStorage.activity_names)
-    for(var i=0;i<activity.length;i++)
-    if(activity[i]["count"]==0)
-    {
-
-        $("#start").text("开始")
-    }
-}
-
-/*function  judge_button_enable()
-{
-    var activity=JSON.parse(localStorage.activity_names)
+    window.location="#activity_list_page"
     for(var i=0;i<activity.length;i++)
     {
         if(activity[i]["count"]==1)
         {
-            try
-            {
-                $("#start_button").button("disable")
-                $("#create_act_button").button("disable")
-            }  catch (e){}
+            console.log("create_activity_btn_disable()")
+            $("#create_activity_button").button("disable")
+            break;
+
+        }
+        else
+        {
+            $("#create_activity_button").button("enable")
         }
     }
-  */
-/*function click_end_button_to_disable()
+
+}
+
+function judge_button_name()
 {
-    var activity=JSON.parse(localStorage.activity_names)
-    var prompt=confirm("确定要结束本活动" + '\n' + "报名吗?")
-    if(prompt==true)
+    if(localStorage.now_activity_page_count==0)
     {
-        try
-        {
-        $("#start_button").button("disable")
-        } catch (e) {}
-        activity[a]["count"]=2;
+        click_start_to_end_button()
+    }
+    else if(localStorage.now_activity_page_count==1)
+    {
+        confirm_activity_end_window()
     }
 }
-        /*   activity_count=show_activity_end_window()
-        }
-        if(activity_count==1)
-        {
-               activity[i]["count"]=2;
-        }   */
-
-
-
-
-/*function confirm_activity_end_window()
+function confirm_activity_end_window()
 {
+    var activity=JSON.parse(localStorage.activity_names)
+    for(var i=0;i<activity.length;i++)
+    {
+
+    }
     var prompt=confirm("确定要结束本活动" + '\n' + "报名吗?")
     if(prompt==true)
     {   try
         {
-            $("#start_button").button("disable")
+            $("#start_activity_button").button("disable")
         } catch (e) {}
-        return   1;
+        for(var i=0;i<activity.length;i++)
+        {
+           if(activity[i]["count"]==1)
+           {
+               activity[i]["count"]=2
+               localStorage.activity_names=JSON.stringify(activity)
+               console.log(" for(var i=0;i<activity.length;i++)")
+           }
+        }
     }
-    return    0 ;
-}  */
+}
 
 
 
